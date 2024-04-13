fbbc27818fb43b3ca8baa31181c82482c80a74f3
alexandrubuzea <alexandru.buzea2007@stud.acs.upb.ro>
2024-04-14 01:27:06 +0300

    added server skel

--- server/HelloWorld.ts
+++ server/HelloWorld.ts
@@ -1,11 +1,43 @@
 // highlight-next-line
 import { GenezioDeploy } from "@genezio/types";
 
+// make our transaction datatype
+
+export type Transaction = {
+	from : string;
+	to : string;
+	amount : number;
+}
+
+let group : any = {}
+
 // highlight-next-line
 @GenezioDeploy()
-export class HelloWorldClass {
-  hello(name: string): string {
-    console.log("DEBUG: Call hello method");
-    return "Hello " + name;
-  }
+export class ServerClass {
+	async postTransaction(tr : Transaction) : Promise<boolean> {
+		const {from, to, amount} = tr;
+
+		if (group[from] !== undefined)
+			return false
+
+		group[from] = to
+
+		// TODO: blockchain / create a contract
+		return true
+	}
+
+	async sendNotification(inheritor : string) : Promise<any> {
+		return null
+	}
+
+	async acceptInheritance(tr : Transaction) : Promise<boolean> {
+		const {from, to, amount} = tr
+		
+		if (from in group) {
+			group.erase(from)
+			return true
+		}
+	
+		return false
+	}
 }
