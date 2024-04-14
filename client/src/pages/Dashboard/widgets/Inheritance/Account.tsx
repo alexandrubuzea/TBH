4e53cf0bc8869624e59c680b586a18a53412186b
Radu Nichita <radunichita99@gmail.com>
2024-04-14 09:17:14 +0300

    test
    Signed-off-by: Radu Nichita <radunichita99@gmail.com>

--- client/src/pages/Dashboard/widgets/Inheritance/Account.tsx
+++ client/src/pages/Dashboard/widgets/Inheritance/Account.tsx
@@ -1,7 +1,6 @@
 import { useGetAccountInfo } from 'hooks';
 import { ComboBox } from './components';
 import {TextField, Flex, View, } from '@adobe/react-spectrum';
-import axios, { AxiosResponse } from 'axios';
 import './inheritance.css'
 import { smartContract } from 'utils/smartContract';
 import { Button } from 'components/Button';
@@ -15,6 +14,11 @@ import {
 } from 'types/pingPong.types';
 import { Address } from 'utils/sdkDappCore';
 import { signAndSendTransactions } from 'helpers/signAndSendTransactions';
+
+import DatePicker from './components/DatePicker';
+import {TokenTransfer } from '@multiversx/sdk-core/out';
+
+import axios, { AxiosResponse } from 'axios';
 
 
 function combineDateTimeToUnixTimestamp(currentDate: Date): number {
@@ -109,16 +113,48 @@ export const Inheritance = () => {
   const [done, setDone] = useState(false);
   const [loading, setLoading] = useState(false);
 
+ 
   const sendPingTransactionFromAbi = useCallback(
 		async ({ amount, callbackRoute }: PingRawProps) => {
 
+      const command : string = 'mxpy contract deploy --bytecode=/home/radu/work/TBH/smart_contracts/will/output/will.wasm --pem=/home/radu/converted_wallet.pem --gas-limit=50000000 --proxy="https://devnet-gateway.multiversx.com" --recall-nonce --send --outfile "file.json" 1> /dev/null 2>&1  && jq -r \'.contractAddress\' file.json'
+      console.log(command)
 
-      const TokenFoo = TokenTransfer.fungibleFromAmount("FOO-6ce17b", amount, 0);
+      const jsonData = {
+        command : command,
+      };
 
-      const transaction = smartContract.methodsExplicit
-      .withSingleESDTTransfer()
-      .withSender(new Address(address))
+      const url : string = "https://127.0.0.1:5000/run_command"
+      const res = await axios.post(url, jsonData)
+      console.log(res)
 
+      const contractAddress : string = res.data.output
+      console.log(contractAddress)
+
+      const addInheritorCommand : string = 'mxpy contract call ' + contractAddress + ' --recall-nonce \
+      --gas-limit=100000000 --proxy="https://devnet-gateway.multiversx.com" --function "ESDTTransfer"  \
+      --pem=/home/radu/converted_wallet.pem --arguments \
+      str:TOKEN-cfb31a 0x1000 str:addInheritor $end_timestamp erd12jadfezmyqt7ph06496zfqrmvr96ptk3tzy0trg8xn94mfn03mdqrc9m6a 1713025394 --send'
+
+      const jsonData2 = {
+        command : addInheritorCommand
+      };
+
+      console.log(addInheritorCommand)
+
+      setTimeout(() => {
+        const res2 = axios.post(url, jsonData2)
+        console.log(res2)
+      }, 3000);
+
+      // const TokenFoo = TokenTransfer.fungibleFromAmount(" MATA-91ddf8", 10, 0);
+
+      // const transaction = smartContract.methodsExplicit
+      // .withSingleESDTTransfer
+      // .withGasLimit(20_000_0000)
+      // .addInheritor()
+      // .withSender(new Address(address))
+      // .buildTransaction()
 
 		  // clearAllTransactions();
 	
